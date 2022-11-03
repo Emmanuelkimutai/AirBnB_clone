3ec501e131e8b085a1bba9d125946ff6b7fe25cb
Emmanuelkimutai <emmanuelkimutai64@gmail.com>
2022-11-03 22:48:02 +0300

    update on upload file

--- models/base_model.py
+++ models/base_model.py
@@ -1,39 +1,51 @@
 #!/usr/bin/python3
-import uuid
-from datetime import datetime
+"""
+Contains class BaseModel
+"""
 
-"""Base Model"""
+from datetime import datetime
+import models
+import uuid
+
+time = "%Y-%m-%dT%H:%M:%S.%f"
 
 
 class BaseModel:
-    """BaseModel Class Details"""
+    """The BaseModel class from which future classes will be derived"""
+
     def __init__(self, *args, **kwargs):
+        """Initialization of the base model"""
         if kwargs:
             for key, value in kwargs.items():
-                if key == "created_at" or key == "updated_at":
-                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                if key == "__class__":
-                    continue
-                setattr(self, key, value)
+                if key != "__class__":
+                    setattr(self, key, value)
+            if hasattr(self, "created_at") and type(self.created_at) is str:
+                self.created_at = datetime.strptime(kwargs["created_at"], time)
+            if hasattr(self, "updated_at") and type(self.updated_at) is str:
+                self.updated_at = datetime.strptime(kwargs["updated_at"], time)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            self.updated_at = self.created_at
+            models.storage.new(self)
+            models.storage.save()
 
     def __str__(self):
-        """Prints in the form: [<class name>] (<self.id>) <self.__dict__>"""
-        tmp = '[{}] ({}) {}'
-        return tmp.format(self.__class__.__name__, self.id, self.__dict__)
+        """String representation of the BaseModel class"""
+        return "[{:s}] ({:s}) {}".format(self.__class__.__name__, self.id,
+                                         self.__dict__)
 
     def save(self):
-        """Updates updated_at with current time"""
+        """updates the attribute 'updated_at' with the current datetime"""
         self.updated_at = datetime.now()
-        return
+        models.storage.save()
 
     def to_dict(self):
-        """Return a dictionary containing all keys/values of the instance"""
-        new_dictionary = self.__dict__.copy()
-        new_dictionary.update({'__class__': str(self.__class__.__name__)})
-        new_dictionary["created_at"] = self.created_at.isoformat()
-        new_dictionary["updated_at"] = self.updated_at.isoformat()
-        return new_dictionary
+        """returns a dictionary containing all keys/values of the instance"""
+        new_dict = self.__dict__.copy()
+        if "created_at" in new_dict:
+            new_dict["created_at"] = new_dict["created_at"].strftime(time)
+        if "updated_at" in new_dict:
+            new_dict["updated_at"] = new_dict["updated_at"].strftime(time)
+        new_dict["__class__"] = self.__class__.__name__
+        return new_dict
--- models/state.py
+++ models/state.py
@@ -1,12 +1,12 @@
-#!/usr/bin/python3
-"""Defines the State class."""
+#!/usr/bin/python
+""" holds class State"""
 from models.base_model import BaseModel
 
 
 class State(BaseModel):
-    """Represent a state.
-    Attributes:
-        name (str): The name of the state.
-    """
-
+    """Representation of state """
     name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes state"""
+        super().__init__(*args, **kwargs)
